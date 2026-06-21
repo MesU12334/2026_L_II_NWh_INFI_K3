1e5c4027e592ca1a657f4b8f3e105ab41e505b3e
Max <you@example.com>
2026-06-21 10:03:20 +0200

    Fix lint errors

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,7 +1,7 @@
-from hello_world import app
-from hello_world.formater import get_formatted
-from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
+
+from hello_world import app
+from hello_world.formater import PLAIN, SUPPORTED, get_formatted
 
 moje_imie = "Maksym"
 msg = "Hello Maksym!"
@@ -12,8 +12,13 @@ def index():
     output = request.args.get('output')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
-                         output.lower())
+
+    return get_formatted(
+        msg,
+        moje_imie,
+        output.lower()
+    )
+
 
 @app.route('/outputs')
 def supported_output():
